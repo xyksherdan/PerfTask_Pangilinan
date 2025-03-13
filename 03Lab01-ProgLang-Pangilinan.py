fece0a7b0f2aa92a3f4a7b17917c144e5089462c
xyksherdan <xyksherdan@gmail.com>
2025-03-13 10:47:19 +0800

    Update 03Lab01-ProgLang-Pangilinan.py

--- 03Lab01-ProgLang-Pangilinan.py
+++ 03Lab01-ProgLang-Pangilinan.py
@@ -1,110 +1,105 @@
 class Token:
-    def __init__(self, type_, value):
+    # Token types
+    INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
+
+    def _init__(self, type, value):
         self.type = type_
         self.value = value
 
-    def __str__(self):
-        return f"Token({self.type}, {self.value})"
-
     def __repr__(self):
-        return self.__str__()
-
-
-# Token types
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
-
-
-class Lexer:
-    def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.current_char = self.text[self.pos]
-
-    def error(self):
-        raise Exception('Invalid character')
-
-    def advance(self):
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None  # End of input
-        else:
-            self.current_char = self.text[self.pos]
-
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-    def integer(self):
-        result = ""
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
-
-    def get_next_token(self):
-        while self.current_char is not None:
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-            if self.current_char.isdigit():
-                return Token(INTEGER, self.integer())
-            if self.current_char == '+':
-                self.advance()
-                return Token(PLUS, '+')
-            if self.current_char == '-':
-                self.advance()
-                return Token(MINUS, '-')
-            self.error()
-        return Token(EOF, None)
+        return f'Token({self.type}, {self.value})'
 
 
 class Interpreter:
     def __init__(self, text):
-        self.lexer = Lexer(text)
-        self.current_token = self.lexer.get_next_token()
+        self.text = text  # Input text
+        self.pos = 0  # Current position in the text
+        self.current_token = None  # Current token instance
 
     def error(self):
+        # Error handling for invalid syntax
         raise Exception('Invalid syntax')
 
+    def get_next_token(self):
+        """
+        Lexical analyzer (tokenizer):
+        Breaks input into tokens (INTEGER, MUL, DIV, EOF)
+        """
+        text = self.text
+        if self.pos >= len(text):
+            return Token(Token.EOF, None)
+
+        current_char = text[self.pos]
+        
+        if current_char.isdigit():
+            # Extract multi-digit integer
+            num_value = ''
+            while self.pos < len(text) and text[self.pos].isdigit():
+                num_value += text[self.pos]
+                self.pos += 1
+            return Token(Token.INTEGER, int(num_value))
+
+        if current_char == '*':
+            self.pos += 1
+            return Token(Token.MUL, '*')
+
+        if current_char == '/':
+            self.pos += 1
+            return Token(Token.DIV, '/')
+        
+        self.error()
+
     def eat(self, token_type):
+        """
+        Verify token type and move to next token
+        """
         if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
+            self.current_token = self.get_next_token()
         else:
             self.error()
 
+    def factor(self):
+        """
+        factor : INTEGER
+        """
+        token = self.current_token
+        self.eat(Token.INTEGER)
+        return token.value
+
     def expr(self):
-        # Expect an INTEGER
-        left = self.current_token
-        self.eat(INTEGER)
+        """
+        expr : factor ((MUL | DIV) factor)*
+        """
+        self.current_token = self.get_next_token()
+        result = self.factor()
 
-        # Expect either PLUS or MINUS
-        op = self.current_token
-        if op.type == PLUS:
-            self.eat(PLUS)
-        elif op.type == MINUS:
-            self.eat(MINUS)
-
-        # Expect another INTEGER
-        right = self.current_token
-        self.eat(INTEGER)
-
-        # Perform the calculation and return result
-        if op.type == PLUS:
-            return left.value + right.value
-        else:
-            return left.value - right.value
+        while self.current_token.type in (Token.MUL, Token.DIV):
+            token = self.current_token
+            if token.type == Token.MUL:
+                self.eat(Token.MUL)
+                result *= self.factor()
+            elif token.type == Token.DIV:
+                self.eat(Token.DIV)
+                result /= self.factor()
+        
+        return result
 
 
 def main():
+    """
+    Function to take user input and evaluate expressions
+    """
     while True:
         try:
-            text = input("calc> ")
-            interpreter = Interpreter(text)
+            text = input("Enter an expression (or type 'exit' to end session.): ")
+            if text.lower() == 'exit':
+                break
+            interpreter = Interpreter(text.replace(' ', ''))
             result = interpreter.expr()
-            print(result)
+            print(f'Result: {result}')
         except Exception as e:
-            print(e)
+            print(f'Error: {e}')
 
 
-if __name__ == '__main__':
+if _name_ == "_main_":
     main()
